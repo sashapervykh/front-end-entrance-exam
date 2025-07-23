1ec16adfce2d28dc5a4b5d48699a4ee049cd6d46
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-07-23 22:08:50 +0300

    feat: add editing text and saving changes

--- js/main.js
+++ js/main.js
@@ -8,3 +8,36 @@ import '../css/tools-section.css';
 import '../css/education-section.css';
 import '../css/interest-section.css';
 import '../css/contacts-section.css';
+
+const editable = document.querySelectorAll('[contenteditable="true"]');
+
+if (localStorage.length !== 0) {
+  console.log(1);
+  for (let i = 0; i < localStorage.length; i++) {
+    const id = localStorage.key(i);
+    console.log(id);
+    const savedData = localStorage.getItem(id);
+    console.log(savedData);
+    const elem = document.getElementById(id);
+    console.log(elem);
+    if (!elem) continue;
+    if (savedData === '') {
+      console.log('hiding');
+      elem.classList.add('hidden');
+      continue;
+    }
+    if (id.indexOf('hashtags' !== -1)) {
+      elem.replaceChildren();
+      elem.textContent = savedData;
+    }
+    elem.textContent = savedData;
+  }
+}
+
+editable.forEach((elem) => {
+  elem.addEventListener('input', (event) => {
+    const changedElement = event.target;
+    const changedElementID = changedElement.id;
+    localStorage.setItem(changedElementID, changedElement.textContent);
+  });
+});
